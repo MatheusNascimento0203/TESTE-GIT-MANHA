3f118fb5cd2258c1df6e9e59f88554efa7d2d890
natandevdasmn <tam.fpf@gmail.com>
2023-09-27 09:47:31 -0300

    Rapuda é doce, mas não é mole, Git é simples, mas é desafiador

--- teste.py
+++
@@ -1,8 +0,0 @@
-#Um teste para saber se a letra é vogal ou consoante
-
-letra = str(input('Digite uma letra: '))
-vogal = ['a', 'e', 'i', 'o', 'u', 'A', 'E', "I", 'O', 'U']
-if letra in vogal:
-  print('A letra digitada é uma vogal.')
-else:
-  print('A letra digitada é uma consoante.')
